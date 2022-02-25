96974611932366d2f33487f624c42d46d9000ec4
t-tokio <tt.tamura0913@gmail.com>
2022-02-25 15:27:54 +0900

    itemsテーブルの作成

---
+++ db/migrate/20220225054420_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item_name, null: false
+      t.text   :text, null: false
+      t.integer :category_id, null: false
+      t.integer :item_state_id, null: false
+      t.integer :shipping_cost_bearer_id, null: false
+      t.integer :prefectures_id, null: false
+      t.integer :shipping_days_id, null: false
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
+    end
+  end
+end

63d0d95dac3981b2a01bdf234155ed3ce14317b9
t-tokio <tt.tamura0913@gmail.com>
2022-02-25 20:10:51 +0900

    ActiveHashの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :item_state
+  belongs_to :shipping_cost_bearer
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   validates :item_name, presence: true
   validates :text, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_state_id, numericality: { other_than: 1 }
-  validates :shipping_cost_bearer_id, numericality: { other_than: 1 }
-  validates :prefectures_id, numericality: { other_than: 1 }
-  validates :shipping_days_id, numericality: { other_than: 1 }
+  validates :item_state_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_bearer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefectures_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_days_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
 end

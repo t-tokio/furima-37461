5ca4a145bdce97b08e4d5e74101f78583f8e10a8
t-tokio <tt.tamura0913@gmail.com>
2022-03-01 00:20:09 +0900

    販売手数料小数点切り捨て設定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,10 @@
 function count (){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const fee = priceInput.value * 0.1 ;
+    const fee = Math.floor(priceInput.value * 0.1) ;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = `${fee}`;
-    const SalesProfit = priceInput.value - fee ;
+    const SalesProfit = Math.floor(priceInput.value - fee) ;
     const profit = document.getElementById("profit");
     profit.innerHTML = `${SalesProfit}`;
   });

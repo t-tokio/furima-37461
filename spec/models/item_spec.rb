767766e656e701516ec0031cd0e58b0a6cbdd058
t-tokio <tt.tamura0913@gmail.com>
2022-03-01 15:45:23 +0900

    Itemモデル単体テストコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,10 +5,101 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
+  describe '商品出品' do
+    context '商品出品ができる場合' do
+      it '全ての入力項目が存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
 
+      it '価格が¥300~¥9,999,999の間であれば出品できる' do
+        @item.price = '10000'
+        expect(@item).to be_valid
+      end
 
+      it '価格入力が半角数値であれば出品できる' do
+        @item.price = '1000'
+        expect(@item).to be_valid
+      end
+    end
 
+    context '商品出品ができない場合' do
+      it 'item_nameが空では出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
 
+      it 'textが空では出品できない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
 
+      it 'category_idが空では出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
 
+      it 'item_state_idが空では出品できない' do
+        @item.item_state_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item state can't be blank")
+      end
+
+      it 'shipping_cost_bearer_idが空では出品できない' do
+        @item.shipping_cost_bearer_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost bearer can't be blank")
+      end
+
+      it 'prefectures_idが空では出品できない' do
+        @item.prefectures_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefectures can't be blank")
+      end
+
+      it 'shipping_days_idが空では出品できない' do
+        @item.shipping_days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping days can't be blank")
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'priceが¥300未満では出品できない' do
+        @item.price = '200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが¥9,999,999未満では出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'priceが半角でなければ出品できない' do
+        @item.price = '１００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'priceが数値でなければ出品できない' do
+        @item.price = '文字'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end

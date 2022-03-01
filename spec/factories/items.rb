c733c856dc55aa4cf6ece416b17074b19ce2e8f8
t-tokio <tt.tamura0913@gmail.com>
2022-03-01 11:45:25 +0900

    ItemモデルFactoryBotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { "名前" }
+    text { "説明" }
+    category_id { 2 }
+    item_state_id { 2 }
+    shipping_cost_bearer_id { 2 }
+    prefectures_id { 2 }
+    shipping_days_id { 2 }
+    price { 10000 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+
+
+
+
+
+
 end

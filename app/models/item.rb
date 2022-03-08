5bcb838db65d9f8fe6e8b97901e8a380940269b6
t-tokio <tt.tamura0913@gmail.com>
2022-03-08 14:28:16 +0900

    ログイン、ログアウト遷移設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :purchases
+  has_one :purchase
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category

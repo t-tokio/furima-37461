7ab453786af9cd111da12b2207e8886069902695
t-tokio <tt.tamura0913@gmail.com>
2022-03-08 14:27:44 +0900

    環境変数設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_1c54feada5f13b783f305a5a");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
